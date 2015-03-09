ce8147dd70b4d49797a5d228a38af20c3f8ce8b0
Bartosz Jankiewicz <bartosz.jankiewicz@gmail.com>
2015-03-09 23:30:13 +0100

    Initial setup

--- src/test/scala/com/bjankie1/scalatest/ButtonTest.scala
+++ src/test/scala/com/bjankie1/scalatest/ButtonTest.scala
@@ -1,10 +1,13 @@
 package com.bjankie1.scalatest
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalacheck.Gen
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.{FlatSpec, Matchers}
 
 
-class ButtonTest extends FlatSpec with Matchers {
+class ButtonTest extends FlatSpec with Matchers with PropertyChecks {
 
+  val smallInteger = Gen.choose(0,100)
 
   "The button" should "be ON after pressing it" in {
     val button = new Button
@@ -16,8 +19,16 @@ class ButtonTest extends FlatSpec with Matchers {
   "The audit log" should "contain set of states" in {
     val button = new Button
     button.press()
-    button.getAudit should contain only(true)
+    button.getAudit should contain only (true)
   }
 
+
+  "The audit log" should "record all changes" in {
+    forAll(smallInteger) { times =>
+      val button = new Button
+      for(_ <- 1 to times) button.press()
+      button.getAudit should have size times
+    }
+  }
 
 }
